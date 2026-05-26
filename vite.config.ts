237f8defecb494f8219e9458a89e4cbedf49f983
ElliotMonde <elliotphua@gmail.com>
2026-05-27 00:58:31 +0800

    added react Helmet for seo, and vite prerender.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import vitePrerender from 'vite-plugin-prerender'
-import path from 'path'
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
-        vitePrerender({
-            staticDir: path.join(__dirname, 'dist'),
-            routes: ['/']
-        })
     ],
     resolve: {
         alias: {
