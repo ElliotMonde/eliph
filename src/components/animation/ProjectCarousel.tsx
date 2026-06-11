0b6098a13bbbd964c0218efd864f114cac001c27
ElliotMonde <elliotphua@gmail.com>
2026-06-11 20:01:03 +0800

    Change large images to webp. format.

--- src/components/animation/ProjectCarousel.tsx
+++ src/components/animation/ProjectCarousel.tsx
@@ -1,42 +1,42 @@
-import { useEffect, useRef } from "react";
+// import { useEffect, useRef } from "react";
 import { projects } from "../../data/experience"
 import ProjectCard from "./ProjectCard"
-import gsap from "gsap"
-import ScrollTrigger from "gsap/ScrollTrigger";
+// import gsap from "gsap"
+// import ScrollTrigger from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
 export default function ProjectCarousel() {
-    const containerRef = useRef<HTMLDivElement | null>(null);
-    const gridItemClass = ".project-grid-item";
+    // const containerRef = useRef<HTMLDivElement | null>(null);
+    // const gridItemClass = ".project-grid-item";
 
-    useEffect(() => {
-        const items = gsap.utils.toArray(gridItemClass);
-        if (!containerRef.current || items.length == 0) return;
+    // useEffect(() => {
+    //     const items = gsap.utils.toArray(gridItemClass);
+    //     if (!containerRef.current || items.length == 0) return;
 
-        const animation = gsap.fromTo(items, {
-            y: 60,
-            opacity: 0,
-        }, {
-            opacity: 1,
-            y: 0,
-            stagger: 0.2,
-            duration: 1.6,
-            ease: "power2.out",
-            scrollTrigger: {
-                trigger: containerRef.current,
-                start: "top bottom",
-                toggleActions: "play none play none"
-            }
-        });
+    //     const animation = gsap.fromTo(items, {
+    //         y: 60,
+    //         opacity: 0,
+    //     }, {
+    //         opacity: 1,
+    //         y: 0,
+    //         stagger: 0.2,
+    //         duration: 1.6,
+    //         ease: "power2.out",
+    //         scrollTrigger: {
+    //             trigger: containerRef.current,
+    //             start: "top bottom",
+    //             toggleActions: "play none play none"
+    //         }
+    //     });
 
-        return () => {
-            animation.scrollTrigger?.kill();
-        }
-    }, []);
+    //     return () => {
+    //         animation.scrollTrigger?.kill();
+    //     }
+    // }, []);
 
     return (
-        <div ref={containerRef} className="grid grid-cols-3 items-center place-content-center justify-center gap-2">
+        <div className="grid grid-cols-3 items-center place-content-center justify-center gap-2">
             {projects.map((project, ind) => (
                 <div key={`project-${ind}`} className={"project-grid-item"}>
                     <ProjectCard {...project} />
