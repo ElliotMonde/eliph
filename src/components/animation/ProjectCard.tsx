af74e155cf1ad82386da605b8d9141da0ff38856
ElliotMonde <elliotphua@gmail.com>
2026-06-12 13:48:39 +0800

    Lazy load images.

--- src/components/animation/ProjectCard.tsx
+++ src/components/animation/ProjectCard.tsx
@@ -9,8 +9,8 @@ export default function ProjectCard({
 
     return (
         <div
-            style={{ backgroundImage: `url(/${mediaUrl})` }}
-            className={`flex flex-col w-full max-w-87.5 justify-end aspect-square rounded-2xl overflow-hidden bg-cover bg-center relative group cursor-pointer shadow-2xl`}>
+            className={`flex flex-col w-full max-w-87.5 justify-end aspect-square rounded-2xl overflow-hidden relative group cursor-pointer shadow-2xl`}>
+            <img loading="lazy" src={mediaUrl} alt={name} className="absolute w-full h-full z-0 object-cover"/>
             <div className="absolute inset-0 z-0 bg-linear-to-t from-black/70 via-black/10 to-transparent" />
             <div className="truncate mx-3 z-1 text-light"><TextType text={name} theme="h3" /></div>
             <div className="truncate mx-3 mb-3 z-1 text-light"><TextType text={subtitle ? subtitle : ""} theme="caption" /></div>
